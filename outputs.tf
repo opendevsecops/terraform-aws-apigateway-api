137a5e72ba390549293623d240414d20209c1e67
Petko D. Petkov <pdp@gnucitizen.org>
2020-05-22 21:45:21 +0000

    Expose API arn.

--- outputs.tf
+++ outputs.tf
@@ -10,6 +10,10 @@ output "id" {
   value = aws_api_gateway_rest_api.main.id
 }
 
+output "arn" {
+  value = aws_api_gateway_rest_api.main.arn
+}
+
 output "root_resource_id" {
   value = aws_api_gateway_rest_api.main.root_resource_id
 }
