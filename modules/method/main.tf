b9e9782143d04a086bf5d1b6f20383422d2b1920
Petko D. Petkov <pdp@gnucitizen.org>
2020-06-30 11:21:05 +0000

    Removed some TODOs.

--- modules/method/main.tf
+++ modules/method/main.tf
@@ -96,12 +96,6 @@ resource "aws_api_gateway_method_response" "S401" {
   depends_on = [aws_api_gateway_method.method]
 }
 
-# TF-UPGRADE-TODO: In Terraform v0.11 and earlier, it was possible to begin a
-# resource name with a number, but it is no longer possible in Terraform v0.12.
-#
-# Rename the resource and run `terraform state mv` to apply the rename in the
-# state. Detailed information on the `state move` command can be found in the
-# documentation online: https://www.terraform.io/docs/commands/state/mv.html
 resource "aws_api_gateway_integration_response" "S200" {
   count = var.should_create ? 1 : 0
 
@@ -153,12 +147,6 @@ EOF
   ]
 }
 
-# TF-UPGRADE-TODO: In Terraform v0.11 and earlier, it was possible to begin a
-# resource name with a number, but it is no longer possible in Terraform v0.12.
-#
-# Rename the resource and run `terraform state mv` to apply the rename in the
-# state. Detailed information on the `state move` command can be found in the
-# documentation online: https://www.terraform.io/docs/commands/state/mv.html
 resource "aws_api_gateway_integration_response" "S401" {
   count = var.should_create ? 1 : 0
 
